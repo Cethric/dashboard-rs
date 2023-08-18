6aab72a3dc0a9c7fb204369ae9324d695499d839
Blake <blake@localhome.dev>
2023-08-18 17:52:24 +1000

    Update snap and remove unused code

--- build.rs
+++ build.rs
@@ -1,20 +1,4 @@
-use std::io::Write;
-
 fn main() {
     println!("cargo:info=begin generating leptos resources");
-    let root_dir: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let generated_dir = root_dir.join("generated");
-    let js_dir = generated_dir.join("js");
-
-    std::fs::create_dir_all(js_dir.clone()).unwrap();
-    std::fs::File::create(js_dir.join("tiptap-bundle.min.js"))
-        .unwrap()
-        .write_all(leptos_tiptap_build::TIPTAP_BUNDLE_MIN_JS.as_bytes())
-        .unwrap();
-
-    std::fs::File::create(js_dir.join("tiptap.js"))
-        .unwrap()
-        .write_all(leptos_tiptap_build::TIPTAP_JS.as_bytes())
-        .unwrap();
     println!("cargo:info=end generating leptos resources");
 }
